2d0dfc11e48260615848846aacf23f1b4d28c589
cassieyrh <cassieyrh@users.noreply.github.com>
2015-06-30 00:04:10 -0400

    Update vigenere.c

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -3,7 +3,7 @@
     
     Author: Ruiheng Yin
     
-    This is a solution for the vigenere problem in pset2 of CS50x.
+    This is a solution for the vigenere problem in pset2 of CS50x3.
     This file is a a program that encrypts messages using Vigenere's cipher.
     
 */
